2b39f15ee30a905a753d88b64cc1a6e80a1be470
anuup22 <anuup22.cs@gmail.com>
2023-09-25 22:09:08 +0530

    exp3 changed

--- exp3.cpp
+++ exp3.cpp
@@ -1,36 +1,16 @@
 #include<iostream>
 using namespace std;
 
-class Student{
-    string name;
-    int roll;
-    float cgpa;
+class DB;
+class DM{
+    
+};
 
-    public:
-    void setData(){
-        cout<<"Enter name: ";
-        cin>>name;
+class DB{
 
-        cout<<"Enter roll no.: ";
-        cin>>roll;
-
-        cout<<"Enter CGPA: ";
-        cin>>cgpa;
-    }   
-    void getData(){
-        cout<<endl;
-        cout<<"My name is "<<name<<endl;
-        cout<<"My roll num is "<<roll<<endl;
-        cout<<"My CGPA is "<<cgpa;
-    }
 };
 
 
 int main(){
-    Student s1;
 
-    s1.setData();
-    s1.getData();
-
-    return 0;
 }
